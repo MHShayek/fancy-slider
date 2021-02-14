82e1bc56b85bb9fcb22e8bade66ac5553d54c25c
MHShayek <mahedihassanshayek@gmail.com>
2021-02-14 22:07:23 +0600

    slider complete

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-// selected image 
+// selected image  area
 let sliders = [];
 
 
@@ -13,43 +13,58 @@ let sliders = [];
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
 
-// show images 
+
+//search by enter key on keyboard area
+const searchButton = document.getElementById("search-btn");
+const searchField = document.getElementById("search");
+
+searchField.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    searchButton.click();
+  }
+});
+
+// show images area
 const showImages = (images) => {
   imagesArea.style.display = 'block';
   gallery.innerHTML = '';
-  // show gallery title
+  // show gallery title area
   galleryHeader.style.display = 'flex';
   images.forEach(image => {
     let div = document.createElement('div');
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
-    gallery.appendChild(div)
+    gallery.appendChild(div);
   })
-
+  toggleSpinner();
 }
 
 const getImages = (query) => {
+  toggleSpinner();
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => {
+      showImages(data.hits);
+    })
     .catch(err => console.log(err))
 }
 
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
- 
+  element.classList.toggle('added');
+
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
+  }
+  else {
+    sliders.pop(img);
   }
 }
 var timer
 const createSlider = () => {
-  // check slider image length
+  // check slider image length area
   if (sliders.length < 2) {
     alert('Select at least 2 image.')
     return;
@@ -71,7 +86,7 @@ const createSlider = () => {
   sliders.forEach(slide => {
     let item = document.createElement('div')
     item.className = "slider-item";
-    item.innerHTML = `<img class="w-100"
+    item.innerHTML = `<img class="itemImage w-100"
     src="${slide}"
     alt="">`;
     sliderContainer.appendChild(item)
@@ -83,14 +98,22 @@ const createSlider = () => {
   }, duration);
 }
 
-// change slider index 
+//resize image slider area
+const sliderSize = () => {
+  const givenSizeWidth = document.getElementById("slider-size-width").value || "600";
+  const resizeSlider = document.querySelector("#sliders");
+  const widthSizePixels = parseInt(givenSizeWidth);
+
+  resizeSlider.style.width = widthSizePixels + "px";
+}
+
+// change slider index area
 const changeItem = index => {
   changeSlide(slideIndex += index);
 }
 
-// change slide item
+// change slide item area
 const changeSlide = (index) => {
-
   const items = document.querySelectorAll('.slider-item');
   if (index < 0) {
     slideIndex = items.length - 1
@@ -118,5 +141,18 @@ searchBtn.addEventListener('click', function () {
 })
 
 sliderBtn.addEventListener('click', function () {
-  createSlider()
+  const duration = document.getElementById('duration').value || 1000;
+  if (duration < 1000) {
+    alert("PLease enter positive value!");
+  }
+  else {
+    createSlider();
+    sliderSize();
+  }
 })
+
+//spinner area
+const toggleSpinner = () => {
+  const spinner = document.getElementById("loading-spinner");
+  spinner.classList.toggle("d-none");
+}
